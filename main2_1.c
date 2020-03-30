6f1903708850e355190142c937b9122a535ac2d4
aimanyounises1 <aymanyounis@icloud.com>
2020-03-30 08:02:50 +0300

    added main2_1.c Parents and child proccesses

---
+++ main2_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include<sys/types.h>
+void print (const char *text){
+    printf("hello form %s\n",text);
+}
+int main(){
+    // create child 
+    pid_t pid = fork();
+    if(pid == 0){
+        // we are in child
+        // create a grand child
+        pid_t pid = fork();
+        if (pid == 0){
+            // we are in the grand child 
+            char * args[2] = {"./friend",NULL};
+            execvp(args[0],args);
+
+        }else{
+            print("child");
+        }
+    }else{
+        print("parent");
+    }
+    return 0;
+}
